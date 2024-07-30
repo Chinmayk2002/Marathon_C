ebb13a71508e7f4c57002cf7c1415c8001274e5f
chinmaykamble <chinmaykamble@gmail.com>
2024-07-30 16:10:02 +0530

    bare metal changes

--- Question1/empty.c
+++ Question1/empty.c
@@ -34,28 +34,99 @@
 #include "ti/driverlib/m0p/dl_core.h"
 #include "ti_msp_dl_config.h"
 #include <machine/_stdint.h>
-#define DELAY (10000000)
+#include <ti/devices/msp/msp.h>
+#include <ti/driverlib/driverlib.h>
 
+void SYSCFG_DL_init(void);
+void SYSCFG_DL_initPower(void);
+void SYSCFG_DL_GPIO_init(void);
+void SYSCFG_DL_SYSCTL_init(void);
+
+#define SYSCTL_SYSOSCCFG_FREQ_SYSOSCBASE         ((uint32_t)0x00000000U)
+#define DL_SYSCTL_SYSOSC_FREQ_BASE  (SYSCTL_SYSOSCCFG_FREQ_SYSOSCBASE)
+#define DL_SYSCTL_setSYSOSCFreq(DL_SYSCTL_SYSOSC_FREQ_BASE)
+#define SYSCTL_BORTHRESHOLD_LEVEL_BORMIN         ((uint32_t)0x00000000U)
+#define DL_SYSCTL_BOR_THRESHOLD_LEVEL_0  (SYSCTL_BORTHRESHOLD_LEVEL_BORMIN)
+#define DL_SYSCTL_setBORThreshold(DL_SYSCTL_BOR_THRESHOLD_LEVEL_0)
+#define POWER_STARTUP_DELAY                                                (16)
+#define GPIOB_BASE (0x400A2000U)
+
+#define GPIO_PORT (GPIOB)
+#define DL_GPIO_PIN_22                                              (0x00400000)
+#define GPIO_LED1_PIN                                           (DL_GPIO_PIN_22)
+#define GPIO_LED1_IOMUX                                          (IOMUX_PINCM50)
+#define DL_GPIO_PIN_26                                              (0x04000000)
+#define GPIO_LED2_PIN                                           (DL_GPIO_PIN_26)
+#define GPIO_LED2_IOMUX                                          (IOMUX_PINCM57)
+#define DL_SYSCTL_MCLK_DIVIDER_DISABLE  (0x0)
+#define SYSCTL_MCLKCFG_UDIV_NODIVIDE             ((uint32_t)0x00000000U)
+#define DL_SYSCTL_ULPCLK_DIV_1  (SYSCTL_MCLKCFG_UDIV_NODIVIDE)
+
+#define DELAY (10000000)
 
 int main(void)
 {
     SYSCFG_DL_init();
     uint32_t counter =0;
-    uint32_t flag =0 ;
     while (1) {
-        switch (flag){
-            case 1: 
+        if(counter%2==1){
             DL_GPIO_clearPins(GPIO_PORT, GPIO_LED2_PIN);
             DL_GPIO_togglePins(GPIO_PORT, GPIO_LED1_PIN); 
-            break;
-            case 0: 
+        }else{
             DL_GPIO_clearPins(GPIO_PORT, GPIO_LED1_PIN);
             DL_GPIO_togglePins(GPIO_PORT, GPIO_LED2_PIN);         
-            break;
         }
         counter++;
         delay_cycles(DELAY);
-        flag = counter%2;
     }
+}
+
+
+ void SYSCFG_DL_init(void)
+{
+    SYSCFG_DL_initPower();
+    SYSCFG_DL_GPIO_init();
+    /* Module-Specific Initializations*/
+    SYSCFG_DL_SYSCTL_init();
+}
+
+ void SYSCFG_DL_initPower(void)
+{
+    DL_GPIO_reset(GPIOA);
+    DL_GPIO_reset(GPIOB);
+
+    DL_GPIO_enablePower(GPIOA);
+    DL_GPIO_enablePower(GPIOB);
+    delay_cycles(POWER_STARTUP_DELAY);
+}
+
+ void SYSCFG_DL_GPIO_init(void)
+{
+
+    DL_GPIO_initDigitalOutput(GPIO_LED1_IOMUX);
+
+    DL_GPIO_initDigitalOutput(GPIO_LED2_IOMUX);
+
+    DL_GPIO_clearPins(GPIO_PORT, GPIO_LED1_PIN |
+        GPIO_LED2_PIN);
+    DL_GPIO_enableOutput(GPIO_PORT, GPIO_LED1_PIN |
+        GPIO_LED2_PIN);
+
+}
+
+
+ void SYSCFG_DL_SYSCTL_init(void)
+{
+
+    //Low Power Mode is configured to be SLEEP0
+    DL_SYSCTL_setBORThreshold(DL_SYSCTL_BOR_THRESHOLD_LEVEL_0);
+
+    DL_SYSCTL_setSYSOSCFreq(DL_SYSCTL_SYSOSC_FREQ_BASE);
+    /* Set default configuration */
+    DL_SYSCTL_disableHFXT();
+    DL_SYSCTL_disableSYSPLL();
+    DL_SYSCTL_setULPCLKDivider(DL_SYSCTL_ULPCLK_DIV_1);
+    DL_SYSCTL_setMCLKDivider(DL_SYSCTL_MCLK_DIVIDER_DISABLE);
+
 }
 
